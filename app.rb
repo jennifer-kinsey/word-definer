aa02070e968712912205ed390bdfda5a44488b86
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-04-28 10:12:09 -0700

    adds views and basic content for index.erb, success.erb, words_form.erb, and words.erb.

--- app.rb
+++ app.rb
@@ -9,3 +9,19 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:words_form)
+end
+
+post('/words') do
+  word = params.fetch('word')
+  Word.new({:word => word}).save
+  @words = Word.all
+  erb(:success)
+end
